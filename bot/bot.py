13d849be51162982ecca70568fa8d1495a1f0d4b
MasoNord <trotf233@gmail.com>
2025-04-26 01:34:56 +0300

    Add menu bar

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand, BotCommandScopeDefault
 from aiogram import filters
 from config import API_TOKEN
 from handlers import cmd_start, cmd_search
@@ -13,7 +14,23 @@ dp = Dispatcher()
 dp.message.register(cmd_start, filters.Command("start"))
 dp.message.register(cmd_search, filters.Command("search"))
 
+async def set_commands():
+    commands = [BotCommand(command='start', description='Старт'),
+                BotCommand(command='search', description='Поиск книги по ее названию')]
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
+
+async def start_bot():
+    await set_commands()
+    logging.info("Бот успешно запущен!")
+
+async def stop_bot():
+    await bot.session.close()
+    logging.info("Бот успешно остановлен!")
+
 async def main():
+    dp.startup.register(start_bot)
+    dp.shutdown.register(stop_bot)
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
